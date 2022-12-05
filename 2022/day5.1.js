dfa493e0817ee98c86bc086484d3698d39317477
tartas1995 <tartas1995@gmail.com>
2022-12-05 16:00:43 +0100

    day 5

---
+++ 2022/day5.1.js
@@ -0,0 +1,47 @@
+const fs = require('fs');
+const input = fs.readFileSync('./day5.txt', { encoding:'utf-8'});
+
+let [ crateInput, instructionsInput ] = input.split("\n\n");
+
+const instructions = instructionsInput.split('\n').map((str) => {
+    const match = str.match(/move (\d+) from (\d+) to (\d+)/);
+    return [ 
+        parseInt(match[1], 10), 
+        parseInt(match[2], 10), 
+        parseInt(match[3], 10) 
+    ];
+});
+
+const cratesLines = crateInput.split('\n').slice(0, -1);
+
+const crates = [];
+
+for (let ii = cratesLines.length - 1; ii >= 0; ii--) {
+    const match = cratesLines[ii].match(/(\[[A-Z]\]\s?|   \s?)/g);
+    for (let jj = 0; jj < match.length; jj++) {
+        if (!crates[jj]) {
+            crates[jj] = [];
+        }
+        const letter = match[jj].match(/([A-Z])/g);
+        if (letter !== null) {
+            crates[jj].push(letter[0]);
+        }
+    }
+}
+
+console.log(crates);
+console.log(instructions);
+
+for (let ii = 0; ii < instructions.length; ii++) {
+    const instr = instructions[ii];
+    for (let moves = 0; moves < instr[0]; moves++) {
+        crates[instr[2]-1].push(crates[instr[1]-1].pop())
+    }
+}
+
+console.log(crates);
+let res = '';
+for (let ii = 0; ii < crates.length; ii++) {
+    res += crates[ii].pop();
+}
+console.log(res);
